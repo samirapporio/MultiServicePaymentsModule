9950e0c08a9dcd9502cc7f1826d01d675c2a4b03
Samir Goel <samir@apporio.com>
2020-01-06 12:23:56 +0530

    New activity created for test only

---
+++ paymentmodule/src/main/java/com/apporioinfolabs/paymentmodule/ClonerActivity.java
@@ -0,0 +1,14 @@
+package com.apporioinfolabs.paymentmodule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClonerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cloner);
+    }
+}
