49010a76a8f2815f5c9bf8e0081f1ecc9e78ef65
Samir Goel <samir@apporio.com>
2020-01-06 12:05:50 +0530

    One trial Activty fo testing

---
+++ paymentmodule/src/main/java/com/apporioinfolabs/paymentmodule/TrialActivity.java
@@ -0,0 +1,14 @@
+package com.apporioinfolabs.paymentmodule;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class TrialActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
